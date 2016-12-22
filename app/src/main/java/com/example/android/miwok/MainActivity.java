b322f370619b2bc91297a4a5336bca1bc0c035f6
Jorge Luis Vazquez <jlvazquez825@gmail.com>
2016-12-22 17:25:52 -0500

    ArrayAdapter added NumbersActivity.java

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,12 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +30,55 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        //reference numbers textView
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        //set a click listener on view
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
+
+        //reference colors textView
+        TextView colors = (TextView) findViewById(R.id.colors);
+        //set a click listener on view
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Opening Colors View", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //reference numbers textView
+        TextView family = (TextView) findViewById(R.id.family);
+        //set a click listener on view
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Opening Family View", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //reference numbers textView
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+        //set a click listener on view
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Opening Phrases View", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /*
+    * Send and intent and open numbers activity
+    */
+    public void openNumbersList(View v) {
+        Intent numbersIntent = new Intent(this, NumbersActivity.class);
+        startActivity(numbersIntent);
+
     }
 }
